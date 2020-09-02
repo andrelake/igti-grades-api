cac463cc69bcc2a165906009d0947d9e108a01ee
andrelake <andre.lake@hotmail.com>
2020-09-01 21:32:32 -0300

    Backend atualizado

---
+++ models/gradeModel.js
@@ -0,0 +1,27 @@
+export default (mongoose) => {
+  const schema = mongoose.Schema({
+    name: {
+      type: String,
+      required: true,
+    },
+    subject: {
+      type: String,
+      required: true,
+    },
+    type: {
+      type: String,
+      required: true,
+    },
+    value: {
+      type: Number,
+      required: true,
+    },
+    lastModified: {
+      type: Date,
+    },
+  });
+
+  const Grade = mongoose.model('grade', schema);
+
+  return Grade;
+};
